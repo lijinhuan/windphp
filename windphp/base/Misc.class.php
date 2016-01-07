cca131259601471a252298defbcbe0e19d9dfd52
cyberty <cyberty@gmail.com>
2016-01-08 01:05:39 +0800

    rewrite function getIp using filter_var

--- windphp/base/Misc.class.php
+++ windphp/base/Misc.class.php
@@ -22,21 +22,21 @@ class Misc {
 	public static function getIp() {
 		static $ip = null;
 		if (! $ip) {
-			if (isset ( $_SERVER ['HTTP_X_FORWARDED_FOR'] ) && $_SERVER ['HTTP_X_FORWARDED_FOR'] && $_SERVER ['REMOTE_ADDR']) {
-				if (strstr ( $_SERVER ['HTTP_X_FORWARDED_FOR'], ',' )) {
-					$x = explode ( ',', $_SERVER ['HTTP_X_FORWARDED_FOR'] );
-					$_SERVER ['HTTP_X_FORWARDED_FOR'] = trim ( end ( $x ) );
+			$ip_keys = array('HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'HTTP_CLIENT_IP', 'HTTP_X_CLUSTER_CLIENT_IP', 'REMOTE_ADDR');
+	
+			foreach ($ip_keys as $key) {
+				if (array_key_exists($key, $_SERVER) === true ) {
+					$x = explode(',', $_SERVER[$key]);
+					$tmpip = filter_var(trim(end($x)), FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE);
+							
+					if ($tmpip !== false) {
+						$ip = $tmpip;
+						break;
+					}
 				}
-				if (preg_match ( '/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER ['HTTP_X_FORWARDED_FOR'] )) {
-					$ip = $_SERVER ['HTTP_X_FORWARDED_FOR'];
-				}
-			} elseif (isset ( $_SERVER ['HTTP_CLIENT_IP'] ) && $_SERVER ['HTTP_CLIENT_IP'] && preg_match ( '/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER ['HTTP_CLIENT_IP'] )) {
-				$ip = $_SERVER ['HTTP_CLIENT_IP'];
 			}
-			if (! $ip && preg_match ( '/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER ['REMOTE_ADDR'] )) {
-				$ip = $_SERVER ['REMOTE_ADDR'];
-			}
-			! $ip && $ip = 'Unknown';
+	
+			$ip or $ip = 'Unknown';
 		}
 		return $ip;
 	}
